4df187d714e64dfec02e67412871fc4d6bee38be
John Leinberger <jleinberger@fullsail.edu>
2014-06-25 21:06:23 -0400

    Added second function for changing the separators in a string, no bugs. Awaiting approval for this code.

--- Leinberger_John_Assignment4.js
+++ Leinberger_John_Assignment4.js
@@ -10,11 +10,14 @@ var returnString = " ";
 var inputString1 = "htt://www.aol.com";
 var inputString2 = "http://www.aol.com";
 var inputString3 = "https://www.aol.com";
+var oldString = "dog,cat,bird,lizard";
+var oldSeparator = /,/g;
+var newSeparator = "/";
 var inputNumber1 = 0;
 var inputNumber2 = 0;
 
 //Function #1 - Check to see if the string is a URL
-//
+//finds the location of the 2nd / and runs a for loop to search through the string, creating a new one with those returned items to be used as a comparison
 function urlChecker(inputString){
     //local variables
     var slashLocation = inputString.indexOf("/") + 1;
@@ -31,6 +34,14 @@ function urlChecker(inputString){
       
 };
 
+//Function #2 - Replace the separator in a string
+//globally replaces the original separator with a new one and returns the updated string
+function stringSeparatorSwap(originalString, originalSeparator, newSeparator) {
+    
+    var updatedString = originalString.replace(originalSeparator, newSeparator);
+    
+    return updatedString;
+};
 //main code
 //Checking Function #1 meets all parameters
 returnBoolean = urlChecker(inputString1);
@@ -39,3 +50,8 @@ returnBoolean = urlChecker(inputString2);
 console.log("It is " + returnBoolean + " that " + inputString2 + " is a correct URL");
 returnBoolean = urlChecker(inputString3);
 console.log("It is " + returnBoolean + " that " + inputString3 + " is a correct URL");
+
+//checking Function #2 meets all parameters
+returnString = stringSeparatorSwap(oldString, oldSeparator, newSeparator);
+console.log("The old string was " + oldString);
+console.log("The updated string is " + returnString);
